6848e85461c493c0655cc13c7ad290f7ca92fc05
Antonio Pedro <antoniopedrocorrearodrigues@gmail.com>
2024-05-22 14:27:55 -0300

    changes the fetch url

--- src/js/model.js
+++ src/js/model.js
@@ -4,11 +4,14 @@ export const state = {
 
 export const validateLogin = async function (data) {
   try {
-    const response = await fetch('http://localhost:8080/user/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    });
+    const response = await fetch(
+      'https://smarterbackenddeploy.onrender.com/user/login',
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      }
+    );
 
     if (!response.ok) {
       const result = await response.json();
