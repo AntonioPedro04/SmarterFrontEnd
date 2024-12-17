013f2b72b12fb3fddceaf55bc792b7071105ddeb
Antonio Pedro <antoniopedrocorrearodrigues@gmail.com>
2024-12-17 12:31:17 -0300

    solves signup click through problem

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_URL_AUTH = 'http://localhost:8080/api/v1/auth/';
-export const API_URL = 'http://localhost:8080/';
+// export const API_URL_AUTH_PROD = 'http://localhost:8080/api/v1/auth/';
+// export const API_URL_PROD = 'http://localhost:8080/';
 export const API_URL_PROD = 'https://smarterbackenddeploy.onrender.com/';
 export const API_URL_AUTH_PROD =
   'https://smarterbackenddeploy.onrender.com/api/v1/auth/';
--- src/js/views/homepage/homePageView.js
+++ src/js/views/homepage/homePageView.js
@@ -1,3 +1,5 @@
+import { findFlagUrlByIso3Code } from 'country-flags-svg/dist';
+
 class HomePageView {
   #buttonLearn = document.querySelector('.learn-button');
   #rankListEl = document.querySelector('.rank-list');
@@ -13,12 +15,13 @@ class HomePageView {
   renderRank(rank) {
     console.log(rank);
     rank.forEach((userData) => {
+      console.log(findFlagUrlByIso3Code(userData.country));
       const markup = `<li>
                   <div class="person">
                     <p class="position">${userData.position}</p>
                     <object
                       type="image/svg+xml"
-                      data="https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg"
+                      data="${findFlagUrlByIso3Code(userData.country)}"
                       class=""
                     >
                       Your browser does not support SVG
@@ -43,7 +46,7 @@ class HomePageView {
         <p class="position">${userRank.position ? userRank.position : '-'}</p>
         <object
           type="image/svg+xml"
-          data="https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg"
+          data="${findFlagUrlByIso3Code(userRank.country)}"
           class=""
         >
           Your browser does not support SVG
